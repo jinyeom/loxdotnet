6b636d365e715c67a9a06c7318147fddaf0a3515
Jinseok Yeom <jinyeom95@gmail.com>
2022-10-11 01:12:18 +0000

    Update

--- src/Scanner.cs
+++ src/Scanner.cs
@@ -3,32 +3,37 @@ namespace Lox;
 class Scanner
 {
     readonly string source;
-    readonly List<Token> tokens = new List<Token>();
+    readonly List<Token> tokens;
     int start = 0;
     int current = 0;
     int line = 1;
 
     /// <summary>
-    /// Helper property that indicates whether the scanner has consumed all characters.
-    /// </summary>
-    bool IsAtEnd { get => current >= source.Length; }
-
-    /// <summary>
     /// Scanner constructor.
     /// </summary>
-    /// <param name="source"></param>
+    /// <param name="source">Source to be scanned.</param>
     public Scanner(string source)
     {
         this.source = source;
+        this.tokens = new List<Token>();
     }
-    
+
     /// <summary>
-    /// 
+    /// Helper method that returns true if the scanner has consumed all characters in the source.
     /// </summary>
-    /// <returns></returns>
+    /// <returns>True if the scanner has reached the end.</returns>
+    bool IsAtEnd()
+    {
+        return current >= source.Length;
+    }
+
+    /// <summary>
+    /// Scan tokens until it reaches the end of file.
+    /// </summary>
+    /// <returns>List of tokens.</returns>
     public List<Token> ScanTokens()
     {
-        while (!IsAtEnd)
+        while (!IsAtEnd())
         {
             start = current;
             ScanToken();
@@ -55,9 +60,48 @@ class Scanner
             case '+': AddToken(TokenType.Plus); break;
             case ';': AddToken(TokenType.Semicolon); break;
             case '*': AddToken(TokenType.Star); break;
+            case '!': AddToken(Match('=') ? TokenType.BangEqual : TokenType.Bang); break;
+            case '=': AddToken(Match('=') ? TokenType.EqualEqual : TokenType.Equal); break;
+            case '<': AddToken(Match('=') ? TokenType.LessEqual : TokenType.Less); break;
+            case '>': AddToken(Match('=') ? TokenType.GreaterEqual : TokenType.Greater); break;
+            case '/':
+                if (Match('/'))
+                {
+                    while (Peek() != '\n' && !IsAtEnd())
+                    {
+                        Advance();
+                    }
+                }
+                else
+                {
+                    AddToken(TokenType.Slash);
+                }
+                break;
+            case ' ':
+            case '\r':
+            case '\t':
+                // Ignore whitespaces.
+                break;
+            case '\n':
+                line++;
+                break;
+            case '"':
+                String();
+                break;
             default:
-                // TODO: handle error from a separate class, not Program. 
-                Program.Error(line, $"Unexpected character: {currentCharacter}");
+                if (Char.IsDigit(currentCharacter))
+                {
+                    Number();
+                }
+                else if (Char.IsLetter(currentCharacter))
+                {
+                    Identifier();
+                }
+                else
+                {
+                    // TODO: handle error from a separate class, not Program. 
+                    Program.Error(line, $"Unexpected character: {currentCharacter}");
+                }
                 break;
         }
     }
@@ -65,7 +109,7 @@ class Scanner
     /// <summary>
     /// Helper method that returns the current character and advances to the next character in source.
     /// </summary>
-    /// <returns></returns>
+    /// <returns>Consumed character before advancing the scanner.</returns>
     char Advance()
     {
         return source[current++];
@@ -74,7 +118,7 @@ class Scanner
     /// <summary>
     /// Helper method that adds a new token with the argument token type.
     /// </summary>
-    /// <param name="type"></param>
+    /// <param name="type">Token type.</param>
     void AddToken(TokenType type)
     {
         AddToken(type, null);
@@ -83,11 +127,120 @@ class Scanner
     /// <summary>
     /// Helper method that adds a new token with the argument token type and literal.
     /// </summary>
-    /// <param name="type"></param>
-    /// <param name="literal"></param>
+    /// <param name="type">Token type.</param>
+    /// <param name="literal">Literal object.</param>
     void AddToken(TokenType type, object? literal)
     {
         var text = source[start..current];
         tokens.Add(new Token(type, text, literal, line));
+    }
+
+    /// <summary>
+    /// Helper method that advances to the next character and returns true if the current character matches the argument character.
+    /// </summary>
+    /// <param name="expected">Character to match the current character against.</param>
+    /// <returns>True if the current character matches the argument character, false otherwise.</returns>
+    bool Match(char expected)
+    {
+        if (IsAtEnd())
+        {
+            return false;
+        }
+        if (source[current] != expected)
+        {
+            return false;
+        }
+        current++;
+        return true;
+    }
+
+    /// <summary>
+    /// Helper method that returns the current character.
+    /// </summary>
+    /// <returns>Current character; returns a null character if the scanner is at EOF.</returns>
+    char Peek()
+    {
+        if (IsAtEnd())
+        {
+            return '\0';
+        }
+        return source[current];
+    }
+
+    /// <summary>
+    /// Helper method that adds a string literal token.
+    /// </summary>
+    void String()
+    {
+        while (!IsAtEnd() && Peek() != '"')
+        {
+            if (Peek() == '\n')
+            {
+                line++;
+            }
+            Advance();
+        }
+
+        if (IsAtEnd())
+        {
+            Program.Error(line, "Unterminated string.");
+            return;
+        }
+
+        // The closing ".
+        Advance();
+
+        // Trim the surrounding quotes.
+        var first = start + 1;
+        var last = current - 1;
+        var literal = source[first..last];
+        AddToken(TokenType.String, literal);
+    }
+
+    /// <summary>
+    /// Helper method that adds a number literal token.
+    /// </summary>
+    void Number()
+    {
+        var advanceDigits = () =>
+        {
+            while (Char.IsDigit(Peek()))
+            {
+                Advance();
+            }
+        };
+        advanceDigits();
+        if (Peek() == '.' && Char.IsDigit(PeekNext()))
+        {
+            Advance(); // "."
+            advanceDigits();
+        }
+        var number = Convert.ToDouble(source[start..current]);
+        AddToken(TokenType.Number, number);
+    }
+
+    /// <summary>
+    /// Helper method that returns the next character.
+    /// </summary>
+    /// <returns>Next character.</returns>
+    char PeekNext()
+    {
+        if (current + 1 >= source.Length)
+        {
+            return '\0';
+        }
+        return source[current + 1];
+    }
+
+    /// <summary>
+    /// Helper method that adds an identifier token.
+    /// </summary>
+    void Identifier()
+    {
+        while (Char.IsLetterOrDigit(Peek()))
+        {
+            Advance();
+        }
+        AddToken(TokenType.Identifier);
     }
 }
